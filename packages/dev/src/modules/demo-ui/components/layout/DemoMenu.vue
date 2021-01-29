20a64c8ddf981a3abfb80597bab2ec16f4089fe7
이윤석 <ys84@wemakeprice.com>
2021-01-29 14:44:00 +0900

    [feat] Github Actions

--- packages/dev/src/modules/demo-ui/components/layout/DemoMenu.vue
+++ packages/dev/src/modules/demo-ui/components/layout/DemoMenu.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
     <demo-menu-group :menuList="menuList" />
+    <div class="foot-desc">
+      <div class="desc-box">
+        <i class="icon material-icons">tag_faces</i>
+        <p class="desc">
+          It is being used by
+          <br />
+          <a class="link" href="https://jood-lab.web.app/" target="_blank">
+            jood-lab.web.app
+          </a>
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -23,4 +35,27 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.foot-desc {
+  padding: 20px;
+  .desc-box {
+    display: flex;
+    align-items: center;
+    font-size: 13px;
+    color: #888888;
+    .icon {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 28px;
+      font-size: 28px;
+    }
+    .desc {
+      margin-left: 8px;
+    }
+    .link {
+      color: #2a9e64;
+    }
+  }
+}
+</style>
