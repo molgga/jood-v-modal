66f4a0cfae23530409143ede57ad1d8d2eff48f9
molgga84 <molgga84@gmail.com>
2021-06-08 10:10:25 +0900

    [fix] open-strategy types 수정

--- packages/lib/src/modules/open-strategy/types.ts
+++ packages/lib/src/modules/open-strategy/types.ts
@@ -3,6 +3,7 @@ import { Properties as CSSProperties } from 'csstype';
 export interface OpenStrategyStyleSet {
   modal?: CSSProperties;
   pivot?: CSSProperties;
+  overlay?: CSSProperties;
 }
 
 export interface OpenStrategy {
