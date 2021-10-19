02a2a8cf81ebed6fdb912c4e46471f5a3be458d4
이윤석 <ys84@wemakeprice.com>
2021-10-19 13:46:21 +0900

    pull-down close 기능 추가

--- packages/lib/src/composables/index.ts
+++ packages/lib/src/composables/index.ts
@@ -2,3 +2,4 @@ export * from './useJdModalProviderSetup';
 export * from './useJdModalEntrySetup';
 export * from './useJdModalBeforeLeave';
 export * from './useJdModalScrollPrevent';
+export * from './useJdModalPullDownClose';
