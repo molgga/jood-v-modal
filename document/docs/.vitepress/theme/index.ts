70f18f575bfd15efd995d83dd1b287a2ffcacba9
molgga84 <molgga84@gmail.com>
2022-10-15 22:05:23 +0900

    chore: 불필요한 문서 제거 및 정리

--- document/docs/.vitepress/theme/index.ts
+++ document/docs/.vitepress/theme/index.ts
@@ -3,7 +3,7 @@ import DefaultTheme from 'vitepress/theme';
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 // @ts-ignore
-import DocumentApp from './DocumentApp.vue';
+import DocumentApp from '../DocumentApp.vue';
 
 const theme: Theme = {
   NotFound: DefaultTheme.NotFound,
