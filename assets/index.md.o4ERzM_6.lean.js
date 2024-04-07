24edcc3923635870c1ab7f13c65024b804df6a2e
molgga <molgga@users.noreply.github.com>
2024-04-07 06:54:24 +0000

    deploy: 3a353f3f92db76b7473d4431ae7431af1ae9c8b8

--- assets/index.md.o4ERzM_6.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as a,c as i,R as l}from"./chunks/framework.Io-i1kqv.js";const p=JSON.parse('{"title":"Vue3 modal UI 😎","description":"","frontmatter":{},"headers":[],"relativePath":"index.md","filePath":"index.md"}'),o={name:"index.md"},t=l("",5),d=[t];function r(c,n,_,s,u,h){return a(),i("div",null,d)}const f=e(o,[["render",r]]);export{p as __pageData,f as default};
